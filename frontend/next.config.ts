e64a180c9cd9626c69c8a5da80e3c66575efeaf5
jejeayos <jejeayos@gmail.com>
2026-08-08 21:09:46 +0800

    update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [process.env.ALLOWED_DEV_ORIGIN ?? ""],
+  output:"standalone",
 };
 
 export default nextConfig;
