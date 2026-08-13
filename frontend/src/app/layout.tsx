6379676fba8390b3d66a8ee7fe6823f785045c72
jejeayos <jejeayos@gmail.com>
2026-08-13 23:33:32 +0800

    Add preview image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -43,11 +43,7 @@ export const metadata = {
 }
 
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html
       lang="en"
